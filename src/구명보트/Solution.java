dcc1be4e07e84090523f35266674828d72ab74d4
ksks723 <ksks723@gmail.com>
2020-10-28 16:52:01 +0900

    구명보트시작

--- src/구명보트/Solution.java
+++ src/구명보트/Solution.java
@@ -1,5 +1,15 @@
 package 구명보트;
 
 public class Solution {
-    //구명보트시작  ㅇ
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] pp = {70,50,80,50};
+        int limit = 100;
+        System.out.println(s.solution(pp,limit));
+    }
 }
